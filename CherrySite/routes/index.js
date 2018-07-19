1e81a18601e9bbec5e2f63a392fc1f3499f0a37b
PlasmaSnake <michaelsy34@gmail.com>
2018-07-19 08:04:20 -0400

    Page navigation and calendar works. Calendar is improperly loading.

--- CherrySite/routes/index.js
+++ CherrySite/routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/About', function(req, res, next) {
+  res.render('About');
+});
+
+router.get('/Contact', function(req, res, next) {
+  res.render('Contact');
+});
+
+router.get('/Media', function(req, res, next) {
+  res.render('Media');
+});
+
+router.get('/Archive', function(req, res, next) {
+  res.render('Archive');
+});
+
+// Figure out how to do works (Books/Plays/Articles)
 
 module.exports = router;
