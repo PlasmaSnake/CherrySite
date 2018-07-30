90037bee3bd9a3aea735fd729017355c893401a0
PlasmaSnake <michaelsy34@gmail.com>
2018-07-30 16:48:24 -0400

    inserted comment

--- CherrySite/routes/emails.js
+++ CherrySite/routes/emails.js
@@ -4,9 +4,10 @@ var router = express.Router();
 /* GET email listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  // use layout for testing
 });
 
-router.get('/subscribeEMail', function(req, res, next){
+router.post('/subscribeEMail', function(req, res, next){
 
   res.send('You\'re so cool');
   // make this subscribe emails
